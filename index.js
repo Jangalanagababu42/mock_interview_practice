905d622c249cec5552044619bc0b1f092fc1dd7f
Jangala nagababu <60348426+Jangalanagababu42@users.noreply.github.com>
2022-10-01 18:20:45 +0000

    task3-two ques

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var readLineSync = require('readline-sync');
 var score = 0;
 // user age above 25
-var userReadAge = readLineSync.question("do you think im above 25  ?? ");
+var userQues1 = readLineSync.question("do you think im above 25  ?? ");
+var userQues2 = readLineSync.question("where i am from  ?? ");
+var userAns1 = "yes";
+var userAns2 = "Telangana";
 
-if (userReadAge === "yes") {
+if (userQues1 === userAns1) {
   console.log("u r correct")
   score += 1;
 }
@@ -11,6 +14,15 @@ else {
   console.log("u r wrong")
   score -= 1;
 }
+if (userQues2 === userAns2) {
+  console.log("u r correct")
+  score += 1;
+}
+else {
+  console.log("u r wrong")
+  score -= 1;
+}
+
 console.log("ur score is " + score);
 
 

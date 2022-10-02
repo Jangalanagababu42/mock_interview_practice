8b946d4ce5e5cf618bf76a4ea16d4c5a0fc44cbb
Jangala nagababu <60348426+Jangalanagababu42@users.noreply.github.com>
2022-10-02 15:50:29 +0000

    task4:function of product

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-var readLineSync = require('readline-sync');
-var score = 0;
-// user age above 25
-var userQues1 = readLineSync.question("do you think im above 25  ?? ");
-var userQues2 = readLineSync.question("where i am from  ?? ");
-var userAns1 = "yes";
-var userAns2 = "Telangana";
-
-if (userQues1 === userAns1) {
-  console.log("u r correct")
-  score += 1;
+// function of product
+function mul(numberOne, numberTwo){
+  return numberOne*numberTwo;
 }
-else {
-  console.log("u r wrong")
-  score -= 1;
-}
-if (userQues2 === userAns2) {
-  console.log("u r correct")
-  score += 1;
-}
-else {
-  console.log("u r wrong")
-  score -= 1;
-}
-
-console.log("ur score is " + score);
-
-
+var product=mul(4,5);
+console.log("The product of 4 and 5 is " +product);

384c6cbcac990e9a947e17aabbb3391457cb314a
Jangala nagababu <60348426+Jangalanagababu42@users.noreply.github.com>
2022-10-02 16:27:21 +0000

    task5-  functions with userinput

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-// function of product
-function mul(numberOne, numberTwo){
-  return numberOne*numberTwo;
+// functions with userinput
+var readLineSync=require('readline-sync');
+var score=0;
+function userinput(question,answer){
+  var useranswer=readLineSync.question(question);
+  if(answer===useranswer){
+    console.log("your answer is correct")
+    score+=1;
+  }
+  else{
+     console.log("your answer is wrong")
+    score-=1;
+  }
 }
-var product=mul(4,5);
-console.log("The product of 4 and 5 is " +product);
+var questionone="what is my name?? ";
+var answerone="kushulu";
+var questiontwo="what is my favourite dish?? ";
+var answertwo="biryani";
+userinput(questionone,answerone);
+userinput(questiontwo,answertwo);
+
+if(score>0){
+  console.log("your score is " + score);
+  console.log("keep scoring high ");
+}
+else{
+  console.log("your score is " + score);
+  console.log("better luck next time ");
+}
